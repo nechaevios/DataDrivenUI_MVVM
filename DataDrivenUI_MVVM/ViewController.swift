b86a64475ad9655dd48555219804d3c1bf5a9a90
Sergey <21218903+xchakiex@users.noreply.github.com>
2022-01-25 15:02:05 +0300

    Initial Commit

---
+++ DataDrivenUI_MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DataDrivenUI_MVVM
+//
+//  Created by Nechaev Sergey  on 25.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
